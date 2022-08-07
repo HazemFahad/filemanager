24b2dbf19070c5631dc52d6055c1bf7438d7f205
squarezy <squarezy>
2022-08-07 11:23:39 +0100

    looks for new files now while running and added readme

--- auto.py
+++ auto.py
@@ -2,6 +2,7 @@ from os import scandir, rename
 from os.path import splitext, exists, join
 from shutil import move
 from time import sleep
+import logging
 
 
 from watchdog.observers import Observer
@@ -38,58 +39,59 @@ def move_file(dest, entry, name):
         rename(oldName, newName)
     move(entry, dest)
 
-with scandir(source_dir) as entries:
-    for entry in entries:
-        name = entry.name
-        dest = source_dir
-        if name.endswith('.wav') or name.endswith('.mp3'):
-            dest = dest_dir_music
-            move_file(dest, entry, name)
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
 
-        elif name.endswith('.mov') or name.endswith('.mp4'):
-            dest = dest_dir_videos
-            move_file(dest, entry, name)
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                dest = source_dir
+                if name.endswith('.wav') or name.endswith('.mp3'):
+                    dest = dest_dir_music
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.pdf'):
-            dest = dest_dir_pdf
-            move_file(dest, entry, name)
+                elif name.endswith('.mov') or name.endswith('.mp4'):
+                    dest = dest_dir_videos
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith(".png") or name.endswith(".PNG"):
-            dest = dest_dir_images
-            move_file(dest, entry, name)
+                elif name.endswith('.pdf'):
+                    dest = dest_dir_pdf
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.doc') or name.endswith('.docx') or name.endswith(".xls"):
-            dest = dest_dir_documents
-            move_file(dest, entry, name)
+                elif name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith(".png") or name.endswith(".PNG"):
+                    dest = dest_dir_images
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.dmg'):
-            dest = dest_dir_installers
-            move_file(dest, entry, name)
+                elif name.endswith('.doc') or name.endswith('.docx') or name.endswith(".xls"):
+                    dest = dest_dir_documents
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.zip') or name.endswith('.rar'):
-            dest = dest_dir_zip
-            move_file(dest, entry, name)
+                elif name.endswith('.dmg'):
+                    dest = dest_dir_installers
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.excalidraw'):
-            dest = dest_dir_excalidraw
-            move_file(dest, entry, name)
+                elif name.endswith('.zip') or name.endswith('.rar'):
+                    dest = dest_dir_zip
+                    move_file(dest, entry, name)
 
-        elif name.endswith('.excalidraw'):
-            dest = dest_dir_excalidraw
-            move_file(dest, entry, name)
+                elif name.endswith('.excalidraw'):
+                    dest = dest_dir_excalidraw
+                    move_file(dest, entry, name)
 
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.INFO,
-#                         format='%(asctime)s - %(message)s',
-#                         datefmt='%Y-%m-%d %H:%M:%S')
-#     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-#     event_handler = LoggingEventHandler()
-#     observer = Observer()
-#     observer.schedule(event_handler, path, recursive=True)
-#     observer.start()
-#     try:
-#         while True:
-#             time.sleep(1)
-#     finally:
-#         observer.stop()
-#         observer.join()
+           
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
